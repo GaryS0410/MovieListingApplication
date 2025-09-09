26c64e9d490872db22d501594c012064f0f7cdc0
GaryS0410 <garyjsmith0410@gmail.com>
2025-09-09 19:52:39 +0100

    Added register user static method

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -7,5 +7,12 @@ export const loginUser = async (req, res) => {
 
 // Register user 
 export const registerUser = async (req, res) => {
-    res.json({mssg: 'Registered new user'});
+    const  { email, password } = req.body;
+
+    try {
+        const user = await User.register(email, password);
+        res.status(200).json({ email, user });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    };
 }
